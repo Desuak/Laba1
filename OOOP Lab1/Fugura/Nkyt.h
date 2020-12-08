3e30e28bdcc6855a9057d81b2d91ecd01f7e14cd
Desuak <kirill.desuatnik@gmail.com>
2020-12-08 11:27:31 +0300

    Хранение в Дереве

--- OOOP Lab1/Fugura/Nkyt.h
+++ OOOP Lab1/Fugura/Nkyt.h
@@ -14,10 +14,10 @@ This one comes from the parent file Fugura
 */
 class Nkyt : public Figure {
 private:
-    int R,r,n;
+    int R,r,n,level;
 public:
     Nkyt() : R{ 0}, r{0}, n{0} {}
-    Nkyt(int R, int r, int n) : R{ R }, r{r}, n{n} {}
+    Nkyt(int R, int r, int n, int level) : R{ R }, r{r}, n{n} {}
     float p() override { return n*(2*R)*(sin(180/n)); }
     float s() override { return (n*(2*R)*(sin(180/n))) / r; }
 };
--- OOOP Lab1/Tree.h
+++ OOOP Lab1/Tree.h
@@ -6,11 +6,11 @@
 #include <iostream>
 #include <string>
 #include <cmath>
-struct Node
-{
-    int x;
-    Node *left,*right;
-};
+template <typename T>
+struct Node {
+    T x;
+    Node<T> *left, *right;
+}
 void show(Node *&Tree)
 {
 	if (Tree != NULL)
@@ -29,6 +29,40 @@ void del(Node *&Tree){
 	   Tree = NULL;
 	}
 }
+    /**
+     * @brief I overload the << operator for iostream and display the Shapes in strict order.
+     * @param node a node of Tree
+     */
+void _print(iostream &output, Node *node, int level = 0) {
+        output << '|';
+        for (std::size_t i = 0; i < level; i++) {
+            output << "\t|";
+        }
+        if (node == _null_node) {
+            output << "*\n";
+            return;
+        } else {
+            output << node->x << " (";
+            output << ")\n";
+        }
+        _print(output, node->left, level + 1);
+        _print(output, node->right, level + 1);
+    }
+void Insert(Node *node, int info) {
+    if (node == nullptr) {
+        node = new Node();
+        node->x = x;
+    } else {
+        if (info > root->x) {
+            Insert(root->right, x);
+        } else if (x < root-> x) {
+            Insert(x->right, x);
+        };
+    }
+}
+class Tree {
+  void Insert();
+};
 void add_node(int x,Node *&MyTree)
 {
 	if (NULL == MyTree)
